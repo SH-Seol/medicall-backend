2472e1f121de0a8c77a0c2fae589cbb5aa2e8dd0
Seunghwan Seol <tjftmd7@gmail.com>
2025-08-30 18:25:52 +0900

    feature: 모듈 생성

---
+++ src/main/java/com/medicall_backend/medicall/MedicallApplication.java
@@ -0,0 +1,13 @@
+package com.medicall_backend.medicall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medicall_backend/medicall/MedicallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicall_backend.medicall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
