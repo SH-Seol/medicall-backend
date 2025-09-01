4ffdb42254bcdfb50d23b22c941235f1d2a0cfd4
Seunghwan Seol <tjftmd7@gmail.com>
2025-09-01 15:38:44 +0900

    feature: 모노 레포 내 모듈별 spring application 추가

--- medicall-api-doctor/src/main/java/com/medicall/doctor/DoctorApplication.java
+++ medicall-api-doctor/src/main/java/com/medicall/doctor/DoctorApplication.java
@@ -1,13 +1,13 @@
-package com.medicall_backend.medicall;
+package com.medicall.doctor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MedicallApplication {
+public class DoctorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MedicallApplication.class, args);
+		SpringApplication.run(DoctorApplication.class, args);
 	}
 
 }
