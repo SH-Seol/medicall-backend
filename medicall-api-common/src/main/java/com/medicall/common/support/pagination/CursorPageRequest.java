d14721a2d87d1583401cd3dbbd683238777f6e8b
Seunghwan Seol <tjftmd7@gmail.com>
2025-09-26 14:47:39 +0900

    refactor: pagination 위치 api-common 으로 수정

--- medicall-api-common/src/main/java/com/medicall/common/support/pagination/CursorPageRequest.java
+++ medicall-api-common/src/main/java/com/medicall/common/support/pagination/CursorPageRequest.java
@@ -1,4 +1,4 @@
-package com.doctor.support.pagination;
+package com.medicall.common.support.pagination;
 
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
